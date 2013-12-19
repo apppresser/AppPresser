bed8ea2bc14ad87c8ce7632d1580025a825194e5
Justin Sternberg <justin@dsgnwrks.pro>
2013-12-19 15:35:09 -0500

    add appp.js for setting/getting cookies and redirecting to query-var-ed pages in app mode

---
+++ js/appp-admin.js
@@ -0,0 +1,122 @@
+jQuery(document).ready(function($) {
+
+	$.fn.rotate = function(degrees) {
+		return $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)', '-moz-transform' : 'rotate('+ degrees +'deg)',  '-ms-transform' : 'rotate('+ degrees +'deg)', 'transform' : 'rotate('+ degrees +'deg)'});
+	};
+
+	var deg          = 180;
+	var $context     = $('.apppresser_settings');
+	var $slidepanel  = $( '#slidepanel' );
+	var $show        = $('.show_hidden', $context ).after( ' <span>▾</span>' );
+	var $arrow       = $show.next('span').css({'display':'inline-block'}).rotate(deg);
+	var $navtabs     = $( '.nav-tab', $context );
+	var $tabs        = $( '.appp-tabs', $context );
+	var $ajaxinput   = $('#apppresser--appp_home_page');
+	var $ajaxcontext = $ajaxinput.parents('tr');
+	var $ajaxresults = $('.appp-ajax-results-posts', $ajaxcontext);
+	var $ajaxhelp    = $('.appp-ajax-results-help', $ajaxcontext);
+	var $spinner     = $('.appp-spinner', $ajaxcontext);
+
+
+	$( 'a.help', $context ).tooltip().click( function(e) { e.preventDefault(); } );
+
+	$context
+		.on( 'click', '.show_hidden', function( event ) {
+			event.preventDefault();
+
+			$slidepanel.slideToggle(300);
+			$show.toggleClass('active');
+
+			deg = deg === 180 ? 0 : 180;
+			$arrow.rotate(deg);
+		})
+		.on( 'click', '.nav-tab', function( event ) {
+			event.preventDefault();
+			var $self = $(this);
+			$tabs.hide();
+			$navtabs.removeClass( 'nav-tab-active' );
+
+			// Set new current tab
+			$( '.' + $self.data('selector') ).fadeIn('fast');
+			$self.addClass( 'nav-tab-active' );
+
+			if ( typeof window.history.pushState == 'function' ) {
+			    window.history.pushState( '','', $self.attr( 'href' ) );
+			}
+
+		})
+		// when typing a page name..
+		.on( 'keyup', '#apppresser--appp_home_page', function(event) {
+			// fire our ajax function
+			maybeAjax( $(this), event );
+		}).blur(function() {
+			// when leaving the input
+			setTimeout(function(){
+				// if it's been 2 seconds, hide our spinner
+				$spinner.hide();
+			}, 2000);
+		})
+		// When clicking on a results post, populate our input
+		.on( 'click', '.appp-ajax-results-posts a', function(event) {
+			event.preventDefault();
+			var $self = $(this);
+			// hide our spinner
+			$spinner.hide();
+			// populate post ID to field
+			$ajaxinput.val( $self.data('postid') )/*.focus()*/;
+			// clear our results
+			$ajaxresults.html('');
+			$ajaxhelp.hide();
+
+		});
+
+
+	// function for running our ajax
+	function maybeAjax( obj, e ) {
+		// get typed value
+		var post_search = obj.val();
+		// only proceed if the user's typed more than 2 characters
+		if ( post_search.length < 2 )
+			return;
+
+		// only proceed if the user has pressed a number, letter or backspace
+		if (e.which <= 90 && e.which >= 48 || e.which == 8) {
+			// clear out our results
+			$ajaxresults.html('');
+			$ajaxhelp.hide();
+			// show our spinner
+			$spinner.css({'float':'none'}).show();
+			// and run our ajax function
+			setTimeout(function(){
+
+				// if they haven't typed in 500 ms
+				if ( $ajaxinput.val() == post_search ) {
+					$.ajax({
+						type : 'post',
+						dataType : 'json',
+						url : ajaxurl,
+						data : {
+							'action': 'appp_search_post_handler',
+							'page_title': post_search,
+							'nonce': $('#_wpnonce').val()
+						},
+						success : ajaxSuccess
+					});
+				}
+			}, 500);
+		}
+	}
+
+	function ajaxSuccess(response) {
+		console.log( 'response', response );
+		// if we have a response id
+		if ( typeof response.data !== 'undefined' ) {
+			// hide our spinner
+			$spinner.hide();
+			// and populate our results from ajax response
+			$ajaxresults.html(response.data);
+			$ajaxhelp.show();
+		}
+	}
+
+});
--- js/appp.js
+++ js/appp.js
@@ -1,122 +1,195 @@
-jQuery(document).ready(function($) {
+/**
+ * AppPresser cookie tool
+ * @since 1.0.3
+ * @type  {Object}
+ */
 
-	$.fn.rotate = function(degrees) {
-		return $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)', '-moz-transform' : 'rotate('+ degrees +'deg)',  '-ms-transform' : 'rotate('+ degrees +'deg)', 'transform' : 'rotate('+ degrees +'deg)'});
-	};
+// initiate apppCore var if it hasn't been
+window.apppCore = typeof window.apppCore !== 'undefined' ? window.apppCore : {};
 
-	var deg          = 180;
-	var $context     = $('.apppresser_settings');
-	var $slidepanel  = $( '#slidepanel' );
-	var $show        = $('.show_hidden', $context ).after( ' <span>▾</span>' );
-	var $arrow       = $show.next('span').css({'display':'inline-block'}).rotate(deg);
-	var $navtabs     = $( '.nav-tab', $context );
-	var $tabs        = $( '.appp-tabs', $context );
-	var $ajaxinput   = $('#apppresser--appp_home_page');
-	var $ajaxcontext = $ajaxinput.parents('tr');
-	var $ajaxresults = $('.appp-ajax-results-posts', $ajaxcontext);
-	var $ajaxhelp    = $('.appp-ajax-results-help', $ajaxcontext);
-	var $spinner     = $('.appp-spinner', $ajaxcontext);
+/**
+ * Get things started
+ * @since  1.0.3
+ */
+apppCore.init = function() {
+	// initiate apppCore parameters
+	apppCore._isApp    = typeof apppCore.mobile_browser_theme_switch !== 'undefined' && apppCore.mobile_browser_theme_switch === 'on' ? true : 'not set';
+	apppCore.queryVars = false;
 
+	apppCore.log( 'apppCore', apppCore );
 
-	$( 'a.help', $context ).tooltip().click( function(e) { e.preventDefault(); } );
+	if ( apppCore.isApp( name ) && ! apppCore.QueryVars('appp') ) {
+		// Redirect to query var-ed version
+		window.location.href = apppCore.AddQueryVar( window.location.href, 'appp', 1 )
+	}
+}
 
-	$context
-		.on( 'click', '.show_hidden', function( event ) {
-			event.preventDefault();
+/**
+ * Gets cookie value by name
+ * @since  1.0.3
+ * @param  {string} name Name of cookie to retrieve
+ * @return {string}      Value of cookie if found
+ */
+apppCore.ReadCookie = function(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+};
 
-			$slidepanel.slideToggle(300);
-			$show.toggleClass('active');
+/**
+ * Removes cookie value
+ * @since  1.0.3
+ * @param  {string} name Name of cookie
+ */
+apppCore.EraseCookie = function(name) {
+	if ( apppCore.ReadCookie(name) )
+	   document.cookie = name+'=';
+	apppCore.log(name+' erased.');
+};
 
-			deg = deg === 180 ? 0 : 180;
-			$arrow.rotate(deg);
-		})
-		.on( 'click', '.nav-tab', function( event ) {
-			event.preventDefault();
-			var $self = $(this);
-			$tabs.hide();
-			$navtabs.removeClass( 'nav-tab-active' );
+/**
+ * Deletes cookie reference
+ * @since  1.0.3
+ * @param  {string} name Name of cookie
+ */
+apppCore.DeleteCookie = function(name) {
+	document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+	apppCore.log(name+' deleted.');
+};
 
-			// Set new current tab
-			$( '.' + $self.data('selector') ).fadeIn('fast');
-			$self.addClass( 'nav-tab-active' );
+/**
+ * Set cookie value
+ * @since  1.0.3
+ * @param  {string} name Name of cookie
+ */
+apppCore.SetCookie = function(name, value, expires) {
 
-			if ( typeof window.history.pushState == 'function' ) {
-			    window.history.pushState( '','', $self.attr( 'href' ) );
-			}
+	var cookiestring = [[name, '=', encodeURIComponent( value )].join('')];
+	var expire_time = '';
 
-		})
-		// when typing a page name..
-		.on( 'keyup', '#apppresser--appp_home_page', function(event) {
-			// fire our ajax function
-			maybeAjax( $(this), event );
-		}).blur(function() {
-			// when leaving the input
-			setTimeout(function(){
-				// if it's been 2 seconds, hide our spinner
-				$spinner.hide();
-			}, 2000);
-		})
-		// When clicking on a results post, populate our input
-		.on( 'click', '.appp-ajax-results-posts a', function(event) {
-			event.preventDefault();
-			var $self = $(this);
-			// hide our spinner
-			$spinner.hide();
-			// populate post ID to field
-			$ajaxinput.val( $self.data('postid') )/*.focus()*/;
-			// clear our results
-			$ajaxresults.html('');
-			$ajaxhelp.hide();
+	if ( expires ) {
+		expire_time = new Date();
+		expire_time.setTime( expire_time.getTime() + expires );
+		expire_time = expire_time.toGMTString();
+		cookiestring.push( ['expires=', expire_time ].join('') );
+	}
+	cookiestring = cookiestring.join(';')+';';
+	document.cookie = cookiestring;
+	apppCore.log( 'SetCookie: '+ name +' set to "'+ value +'"', 'Expires?', expire_time );
+};
 
-		});
+/**
+ * Parse current url & return associative array or bool.
+ * @since  1.0.3
+ * @param  {string} Query var to check
+ * @return {mixed}  Returns either bool or full array of query var parts
+ */
+apppCore.QueryVars = function( queryVar ) {
 
+	if ( apppCore.queryVars ) {
+		if ( queryVar )
+			return apppCore.queryVars.hasOwnProperty( queryVar );
+		return apppCore.queryVars;
+	}
+	// string = string ? string : window.location.href;
+	string = window.location.href;
+	var vars = [], hash, parse = string.search(/\?/i);
 
-	// function for running our ajax
-	function maybeAjax( obj, e ) {
-		// get typed value
-		var post_search = obj.val();
-		// only proceed if the user's typed more than 2 characters
-		if ( post_search.length < 2 )
-			return;
-
-		// only proceed if the user has pressed a number, letter or backspace
-		if (e.which <= 90 && e.which >= 48 || e.which == 8) {
-			// clear out our results
-			$ajaxresults.html('');
-			$ajaxhelp.hide();
-			// show our spinner
-			$spinner.css({'float':'none'}).show();
-			// and run our ajax function
-			setTimeout(function(){
-
-				// if they haven't typed in 500 ms
-				if ( $ajaxinput.val() == post_search ) {
-					$.ajax({
-						type : 'post',
-						dataType : 'json',
-						url : ajaxurl,
-						data : {
-							'action': 'appp_search_post_handler',
-							'page_title': post_search,
-							'nonce': $('#_wpnonce').val()
-						},
-						success : ajaxSuccess
-					});
-				}
-			}, 500);
-		}
+	// if we don't find a query string, return false
+	if ( parse === -1 ) {
+		return false;
 	}
 
-	function ajaxSuccess(response) {
-		console.log( 'response', response );
-		// if we have a response id
-		if ( typeof response.data !== 'undefined' ) {
-			// hide our spinner
-			$spinner.hide();
-			// and populate our results from ajax response
-			$ajaxresults.html(response.data);
-			$ajaxhelp.show();
+	// if we do, break the pieces into an array
+	var hashes = string.slice(string.indexOf('?') + 1).split('&');
+	for( var i = 0; i < hashes.length; i++ ) {
+		hash = hashes[i].split('=');
+		vars.push(hash[0]);
+		vars[hash[0]] = hash[1];
+	}
+	apppCore.queryVars = vars;
+
+	// return bool, whether query var exists
+	if ( queryVar )
+		return apppCore.queryVars.hasOwnProperty( queryVar );
+
+	// return the array
+	return apppCore.queryVars;
+};
+
+apppCore.AddQueryVar = function( url, queryVar, value ) {
+    // Using a positive lookahead (?=\=) to find the
+    // given parameter, preceded by a ? or &, and followed
+    // by a = with a value after than (using a non-greedy selector)
+    // and then followed by a & or the end of the string
+    var val = new RegExp('(\\?|\\&)' + queryVar + '=.*?(?=(&|$))'),
+        qstring = /\?.+$/;
+
+    // Check if the parameter exists
+    if ( val.test( url ) ) {
+        // if it does, replace it, using the captured group
+        // to determine & or ? at the beginning
+        return url.replace(val, '$1' + queryVar + '=' + value);
+
+    } else if (qstring.test(url)) {
+        // otherwise, if there is a query string at all
+        // add the param to the end of it
+        return url + '&' + queryVar + '=' + value;
+
+    } else {
+        // if there's no query string, add one
+        return url + '?' + queryVar + '=' + value;
+    }
+}
+
+/**
+ * Safely log things if query var is set or WP_DEBUG or SCRIPT_DEBUG is on
+ * @since  1.0.3
+ */
+apppCore.log = function() {
+	'use strict';
+	if ( ( apppCore.debug || apppCore.QueryVars('appp-debug') ) && console && typeof console.log === 'function' ) {
+		console.log.apply(console, arguments);
+	}
+};
+
+
+/**
+ * Returns number of pageviews cookie value
+ * @since  1.0.3
+ * @param  {string} name Cookie name
+ * @return {int}    Page views count
+ */
+apppCore.isApp = function( name ) {
+
+	if ( apppCore._isApp === 'not set' ) {
+
+		apppCore.cookieName = name ? name : 'AppPresser_Appp';
+
+		// Check query var to Set the is_app cookie
+		if ( apppCore.QueryVars('appp') ) {
+			apppCore.SetCookie(apppCore.cookieName, true, 86400 * 30);
 		}
+
+		// Check query vars for deleting cookie
+		if ( apppCore.QueryVars('erase-AppPresser_Appp') )
+			apppCore.EraseCookie(apppCore.cookieName);
+
+		// get our value
+		var cookie = apppCore.ReadCookie(apppCore.cookieName);
+		apppCore.log( 'isApp: '+ apppCore.cookieName, cookie );
+
+		apppCore._isApp = cookie === true;
+
 	}
 
-});
+	// return our count
+	return apppCore._isApp;
+};
+
+apppCore.init();
