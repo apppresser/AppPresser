8a1aa5d364dda61d26c5e636a04f87903db2f74f
Justin Sternberg <justin@dsgnwrks.pro>
2014-01-07 10:31:41 -0500

    cleanup and enqueue only on our pages

--- inc/admin-settings.php
+++ inc/admin-settings.php
@@ -13,17 +13,17 @@
 class AppPresser_Admin_Settings extends AppPresser {
 
 	// A single instance of this class.
-	public static $instance              = null;
-	public static $page_slug             = 'apppresser_settings';
+	public static $instance       = null;
+	public static $page_slug      = 'apppresser_settings';
 	public static $extensions_slug       = 'apppresser_sub_extensions';
-	public static $help_slug             = 'apppresser_sub_help_support';
-	public static $menu_slug             = '';
+	public static $help_slug      = 'apppresser_sub_help_support';
+	public static $menu_slug      = '';
 	public static $extensions_menu_slug  = '';
-	public static $help_menu_slug        = '';
-	public static $image_inputs          = array();
-	public static $all_fields            = array();
-	public static $admin_tabs            = array();
-	public static $license_keys          = array();
+	public static $help_menu_slug = '';
+	public static $image_inputs   = array();
+	public static $all_fields     = array();
+	public static $admin_tabs     = array();
+	public static $license_keys   = array();
 
 	/**
 	 * Creates or returns an instance of this class.
@@ -84,18 +84,20 @@ class AppPresser_Admin_Settings extends AppPresser {
 	 * @since  1.0.0
 	 */
 	function plugin_menu() {
-		
-		// Create main menu
+		// Create main menu and settings page
 		self::$menu_slug = add_menu_page( __( 'AppPresser', 'apppresser' ), __( 'AppPresser', 'apppresser' ), 'manage_options', self::$page_slug, array( $this, 'settings_page' ) );
-		
 		// Create submenu items
+
+		// Extensions page
 		self::$extensions_menu_slug = add_submenu_page( self::$page_slug, __( 'Extensions', 'apppresser' ), __( 'Extensions', 'apppresser' ), 'manage_options', self::$extensions_slug, array( $this, 'extensions_page' ) );
+		// Help page
 		self::$help_menu_slug = add_submenu_page( self::$page_slug, __( 'Help / Support', 'apppresser' ), __( 'Help / Support', 'apppresser' ), 'manage_options', self::$help_slug, array( $this, 'help_support_page' ) );
 
-
-		// enqueue that shiz
-		add_action( 'admin_enqueue_scripts', array( $this, 'admin_scripts' ) );
-
+		add_action( 'admin_head-' . self::$menu_slug, array( $this, 'admin_head' ) );
+		// enqueue
+		foreach ( array( self::$menu_slug, self::$extensions_menu_slug ) as $slug ) {
+			add_action( 'admin_print_scripts-' . $slug, array( $this, 'admin_scripts' ) );
+		}
 	}
 
 	/**
@@ -106,8 +108,17 @@ class AppPresser_Admin_Settings extends AppPresser {
 		// admin scripts and styles
 		wp_enqueue_script( 'appp-admin', self::$js_url . 'appp-admin.js', array( 'jquery', 'jquery-ui-core', 'jquery-ui-tooltip' ), self::VERSION );
 		wp_enqueue_style( 'jquery-ui-smoothness', self::$css_url . 'smoothness/smoothness.custom.min.css' );
-		wp_enqueue_style( 'appp-admin-styles', self::$css_url . 'appp-admin-styles.css' );
+		wp_enqueue_style( 'appp-admin-styles', self::$css_url . 'appp-admin-styles.css', null, self::VERSION );
 	}
+
+	/**
+	 * Easy hook for adding to the admin_head on the AppPresser settings page
+	 * @since  1.0.0
+	 */
+	function admin_head() {
+		do_action( 'appp_admin_settings_head', self::run() );
+	}
+
 
 	/**
 	 * Register AppPresser Settings with Settings API.
@@ -258,6 +269,7 @@ class AppPresser_Admin_Settings extends AppPresser {
 		if ( appp_get_setting( 'mobile_browser_theme_switch' ) ) {
 			self::add_setting( 'mobile_browser_theme_switch', __( 'Load AppPresser for mobile browsers', 'apppresser' ), array( 'type' => 'checkbox', 'helptext' => __( 'Display AppPresser in mobile browsers such as Safari and Chrome, instead of your normal theme.', 'apppresser' ) ) );
 		}
+
 		self::add_setting( 'admin_theme_switch', __( 'Load AppPresser for Admins Only', 'apppresser' ), array( 'type' => 'checkbox', 'helptext' => __( 'Check this if you want to test your AppPresser app without loading it for visitors to your site.', 'apppresser' ), 'description' => __( '(for testing purposes)', 'apppresser' ) ) );
 		self::add_setting( 'appp_theme', __( 'App only theme', 'apppresser' ), array( 'type' => 'select', 'options' => $this->themes, 'helptext' => __( 'Select which theme you want to be loaded inside the app, such as the AppPresser theme.', 'apppresser' ), 'description' => __( 'must be enabled above', 'apppresser' ) ) );
 		self::add_setting( 'appp_home_page', __( 'Use a unique homepage for your app.', 'apppresser' ), array( 'helptext' => __( 'Allows you to specify which page users will see first when they load up you AppPresser app.', 'apppresser' ), 'description' => __( 'Start typing to search for a page', 'apppresser' ) ) );
@@ -322,40 +334,39 @@ class AppPresser_Admin_Settings extends AppPresser {
 
 	/**
 	 * AppPresser extensions page output
-	 * @since  1.0.x
+	 * @since  1.0.4
 	 */
 	function extensions_page() {
 		$class = self::$page_slug;
 		$class .= self::is_mp6() ? ' mp6' : '';
 		?>
 		<div class="wrap <?php echo $class; ?>">
-            <h2><?php printf( 'AppPresser ' .__( 'Extensions &nbsp;&mdash;&nbsp; %s', 'apppresser' ), '<a href="http://apppresser.com/extensions/?ref=appp" class="button-primary" target="_blank">' . __( 'Browse All Extensions', 'apppresser' ) . '</a>' ); ?></h2>
-            <p><?php _e( 'These extensions extend the functionality of AppPresser.', 'apppresser' ); ?></p>
-            <?php 
-	        // Attempt to pull back our cached feed
-	        $feed = get_transient( 'appp_extensions_feed' );
+			<h2><?php printf( 'AppPresser ' .__( 'Extensions &nbsp;&mdash;&nbsp; %s', 'apppresser' ), '<a href="http://apppresser.com/extensions/?ref=appp" class="button-primary" target="_blank">' . __( 'Browse All Extensions', 'apppresser' ) . '</a>' ); ?></h2>
+			<p><?php _e( 'These extensions extend the functionality of AppPresser.', 'apppresser' ); ?></p>
 
-	        // If we don't have a cached feed, pull back fresh data
-	        if ( empty( $feed ) ) {
+         <?php
+			// Attempt to pull back our cached feed
+			$feed = get_transient( 'appp_extensions_feed' );
+			$fallback = '<div class="error"><p>' . __( 'There was an error retrieving the extensions list. Please try again later.', 'apppresser' ) . '</div>';
 
-	                // Retrieve and parse our feed
-	                $feed = wp_remote_get( 'http://apppresser.com/?feed=addons', array( 'sslverify' => false ) );
-	                if ( ! is_wp_error( $feed ) ) {
-	                        if ( isset( $feed['body'] ) && strlen( $feed['body'] ) > 0 ) {
-	                                $feed = wp_remote_retrieve_body( $feed );
-	                                // Cache our feed for 1 hour
-	                                set_transient( 'appp_extensions_feed', $feed, HOUR_IN_SECONDS );
-	                        }
-	                } else {
-	                        $feed = '<div class="error"><p>' . __( 'There was an error retrieving the extensions list from the server. Please try again later.', 'apppresser' ) . '</div>';
-	                }
-	        }
+			// If we don't have a cached feed, pull back fresh data
+			if ( empty( $feed ) ) {
+				// Retrieve and parse our feed
+				$feed = wp_remote_get( 'http://apppresser.com/?feed=addons', array( 'sslverify' => false ) );
+				if ( ! is_wp_error( $feed ) ) {
+					if ( isset( $feed['body'] ) && strlen( $feed['body'] ) > 0 ) {
+						$feed = wp_remote_retrieve_body( $feed );
+						// Cache our feed for 1 hour
+						set_transient( 'appp_extensions_feed', $feed, HOUR_IN_SECONDS );
+					}
+				}
+			}
 
-	        // display the feed or error message
-	        echo $feed;
-            ?>
-        </div>
-        <?php
+			// display the feed or error message
+			echo $feed && ! is_wp_error( $feed ) ? $feed : $fallback;
+			?>
+     </div>
+     <?php
 
 	}
 
@@ -549,6 +560,15 @@ class AppPresser_Admin_Settings extends AppPresser {
 		// send back our encoded data
 		wp_send_json_success( $return );
 
+	}
+
+	/**
+	 * Returns url to the AppPresser settings page
+	 * @since  1.0.4
+	 * @return string  AppPresser settings page url
+	 */
+	public static function url() {
+		return add_query_arg( 'page', self::$page_slug, admin_url( 'admin.php' ) );
 	}
 
 }
