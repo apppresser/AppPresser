d6a7f4e6c497577190f4655b8afe6c49f1b8edd5
Justin Sternberg <justin@dsgnwrks.pro>
2014-04-11 10:06:50 -0400

    Couple methods to help clean up theme switcher and add inline comments for stated switcher goals

--- inc/AppPresser_Theme_Switcher.php
+++ inc/AppPresser_Theme_Switcher.php
@@ -15,6 +15,18 @@ class AppPresser_Theme_Switcher extends AppPresser {
 	public $appp_theme          = false;
 
 	/**
+	 * Theme Switcher goals:
+	 *
+	 * Switch theme to App-only theme if setting exists. This should happen for:
+	 * * Admins, if "Load AppPresser for Admins Only" setting is checked
+	 * * Viewers of the app
+	 * * (Possibly in the future) Switch theme for mobile viewers of site
+	 *
+	 * Switch theme for app-only theme customizer, and customizer settings need to save properly
+	 *
+	 */
+
+	/**
 	 * Party Started
 	 * @since 1.0.0
 	 */
@@ -52,16 +64,9 @@ class AppPresser_Theme_Switcher extends AppPresser {
 			// check if user is running native app
 			self::is_app()
 			// check if the setting is enabled to view the APP theme as an administrator
-			|| (
-				appp_get_setting( 'admin_theme_switch' ) == 'on'
-				&& current_user_can( 'manage_options' )
-			)
+			|| $this->maybe_admin_switch()
 			// it's not an app but we want to switch the theme for mobile
-			|| (
-				! self::is_app()
-				&& appp_get_setting( 'mobile_browser_theme_switch' ) == 'on'
-				&& wp_is_mobile()
-			)
+			|| $this->maybe_mobile_switch()
 			// If we're previewing the app theme
 			|| $this->is_appp_theme_customizer()
 		);
@@ -76,6 +81,24 @@ class AppPresser_Theme_Switcher extends AppPresser {
 		add_filter( 'option_template', array( $this, 'template_request' ), 5 );
 		add_filter( 'option_stylesheet', array( $this, 'stylesheet_request' ), 5 );
 		add_filter( 'template', array( $this, 'maybe_switch' ) );
+	}
+
+	/**
+	 * Check if the setting is enabled to view the APP theme as an administrator
+	 * @since  1.1.1
+	 * @return bool  True if doing switch
+	 */
+	public function maybe_admin_switch() {
+		return appp_get_setting( 'admin_theme_switch' ) == 'on' && current_user_can( 'manage_options' );
+	}
+
+	/**
+	 * Check if the setting is enabled to view the APP theme for mobile, and we're a mobile viewer
+	 * @since  1.1.1
+	 * @return bool  True if doing switch
+	 */
+	public function maybe_mobile_switch() {
+		return ! self::is_app() && appp_get_setting( 'mobile_browser_theme_switch' ) == 'on' && wp_is_mobile();
 	}
 
 	/**
