4eb9527d15b9f7ef9042b3f72dd2fd0c492fea68
Scott Bolinger <designbyscott@gmail.com>
2015-04-30 10:01:48 -0700

    fix license activation bug

--- inc/plugin-updater.php
+++ inc/plugin-updater.php
@@ -145,7 +145,7 @@ class AppPresser_Updater extends AppPresser {
 			return false;
 
 		// Call the custom API.
-		$response = wp_remote_post( esc_url( add_query_arg( array(
+		$response = wp_remote_post( esc_url_raw( add_query_arg( array(
 			'edd_action'=> 'activate_license',
 			'license' 	=> $license,
 			// 'the_title' filter needed to match EDD's check
