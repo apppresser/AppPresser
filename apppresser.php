c991f580d7d76a6438d8c82913a7a0b2ce45fe28
Mario Shtika <marioshtika@gmail.com>
2019-06-11 20:04:55 +0300

    Show alert that JWT plugin is required for authentication to work

--- apppresser.php
+++ apppresser.php
@@ -144,6 +144,9 @@ class AppPresser {
 			require_once( self::$inc_path . 'AppPresser_SystemInfo.php' );
 		}
 
+		// Include the TGM_Plugin_Activation class.
+		require_once dirname( __FILE__ ) . '/inc/class-tgm-plugin-activation.php';
+		add_action( 'tgmpa_register', array( $this, 'apppresser_register_required_plugins' ) );
 	}
 
 	/**
@@ -713,6 +716,38 @@ class AppPresser {
 		return false;
 	}
 
+	function apppresser_register_required_plugins() {
+        /*
+        * Array of plugin arrays. Required keys are name and slug.
+        * If the source is NOT from the .org repo, then source is also required.
+        */
+        $plugins = array(
+            // Include the JWT Authentication for WP REST API from the WordPress Plugin Repository
+            array(
+                'name'      => 'JWT Authentication for WP REST API',
+                'slug'      => 'jwt-authentication-for-wp-rest-api',
+                'required'  => true
+            )
+        );
+
+        /*
+        * Array of configuration settings
+        */
+        $config = array(
+            'id'           => 'apppresser',            // Unique ID for hashing notices for multiple instances of TGMPA.
+            'default_path' => '',                      // Default absolute path to bundled plugins.
+            'menu'         => 'tgmpa-install-plugins', // Menu slug.
+            'parent_slug'  => 'plugins.php',           // Parent menu slug.
+            'capability'   => 'manage_options',        // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+            'has_notices'  => true,                    // Show admin notices or not.
+            'dismissable'  => false,                   // If false, a user cannot dismiss the nag message.
+            'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+            'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+            'message'      => '',                      // Message to output right before the plugins table.
+        );
+
+        tgmpa( $plugins, $config );
+    }
 }
 
 // Singleton rather than a global.. If they want access, they can use:
