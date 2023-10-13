4a24a682d8ea8379cdd961106dff8db4552363b5
gblessylva@gmail.com <gblessylva@gmail.com>
2023-10-13 06:38:14 +0100

    refactored

--- inc/AppPresser_API_Limit.php
+++ inc/AppPresser_API_Limit.php
@@ -4,23 +4,14 @@ class AppPresser_API_Limit
     public static function get_user_route()
     {
         $current_url = $_SERVER['REQUEST_URI'];
-
-        // Check if the URL matches a REST API route
         $api_base = rest_get_url_prefix();
-
-        // Remove the API base from the URL to get the endpoint
         $current_endpoint = str_replace($api_base, '', $current_url);
-
-        // Remove any leading double slashes (if present)
         $current_endpoint = ltrim($current_endpoint, '/');
-
         return $current_endpoint;
     }
     public static function getRateLimitedData($user_ip)
     {
-        $rateLimitData = get_transient($user_ip . 'limited_data');
-        return $rateLimitData;
-
+        return get_transient($user_ip . 'limited_data');
     }
     public static function saveRateLimitedData($currentMinute, $user_ip)
     {
@@ -28,7 +19,6 @@ class AppPresser_API_Limit
             'minute' => $currentMinute,
             'requests' => 1,
         ];
-
         set_transient($user_ip . 'limited_data', $rateLimitData, 20);
     }
 
@@ -41,20 +31,16 @@ class AppPresser_API_Limit
 
     public static function appresser_api_limit()
     {
-        // Get the user's IP address
         $user_ip = $_SERVER['REMOTE_ADDR'];
-        // How many requests per X minutes
         $limit = 2;
         $requests_per_minute = apply_filters('limited_requests_per_minute', $limit);
         $current_route = self::get_user_route();
-        // Define an array of limited routes
         $default_limited_routes = array(
             'appp/v1/reset-password',
             'appp/v1/login',
             'appp/v1/verify-resend'
         );
         $limited_routes = apply_filters('appresser_limited_routes', $default_limited_routes);
-
         if (in_array($current_route, $limited_routes)) {
             $rateLimitData = self::getRateLimitedData($user_ip);
             $currentMinute = floor(time() / 60);
@@ -77,10 +63,6 @@ class AppPresser_API_Limit
                 self::saveRateLimitedData($currentMinute, $user_ip);
             }
         }
-
     }
-
 }
-
-// Add action hook for appresser_api_limit method
 add_action('rest_api_init', array('AppPresser_API_Limit', 'appresser_api_limit'));
